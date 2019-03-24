10e2f1735a64ba0bca2cadec8cb7b9748e923a21
Douglas Pinheiro <douglas.pinheiro@icloud.com>
2019-03-24 13:45:56 -0300

    DAO com Promises

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -18,14 +18,12 @@ module.exports = (app) => {
     
     app.get('/livros', (req, resp) => {
         const livroDao = new LivroDao(db);
-        livroDao.lista(function (err, resultados) {
-
-            resp.marko(
-                require('../views/livros/lista/lista.marko'),
-                {
-                    livros: resultados
+        livroDao.lista()
+            .then(livros => resp.marko(
+                require('../views/livros/lista/lista.marko'), {
+                    livros
                 }
-            )
-        })
+            ))
+            .catch(erro => console.log(erro))
     })
 }
