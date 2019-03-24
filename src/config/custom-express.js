71ea575e693c593846cf5ae175a5810bfe7192bc
Douglas Pinheiro <douglas.pinheiro@icloud.com>
2019-03-24 16:26:50 -0300

    Corrigindo o middleware

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -5,6 +5,14 @@ const methodOverride = require('method-override');
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+
+
+//middlewares
+app.use('/estatico', express.static('src/app/public'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
 app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
         // look in urlencoded POST bodies and delete it
@@ -12,12 +20,6 @@ app.use(methodOverride(function (req, res) {
         delete req.body._method
         return method
     }
-}))
-
-//middlewares
-app.use('/estatico', express.static('src/app/public'))
-app.use(bodyParser.urlencoded({
-    extended: true
 }))
 //middewares
 const rotas = require('../app/rotas/rotas.js')
