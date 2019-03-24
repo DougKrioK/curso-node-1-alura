3f0019dabad60cbfbcf073af34615c8520ebedc4
Douglas Pinheiro <douglas.pinheiro@icloud.com>
2019-03-24 15:43:48 -0300

    Arquivos estáticos

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -4,9 +4,13 @@ require("marko/express")
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+
+//middlewares
+app.use('/estatico', express.static('src/app/public'))
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+//middewares
 const rotas = require('../app/rotas/rotas.js')
 
 
