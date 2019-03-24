876179346148b93976d4cb06bf4291a0cab14faf
Douglas Pinheiro <douglas.pinheiro@icloud.com>
2019-03-24 12:12:38 -0300

    Encapsulando as rotas

--- server.js
+++ server.js
@@ -1,32 +1,8 @@
 const app = require('./src/config/custom-express.js')
+const rotas = require('../app/rotas/rotas.js')
+rotas(app)
 
 app.listen(3000, function(){
     console.log('Servidor rodando na porta 3000');
     
-})
-
-app.get('/', (req, resp) => {
-    resp.send(`
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Casa do Código</h1>
-            </body>
-        </html>
-    `)
-})
-
-app.get('/livros', (req, resp) => {
-    resp.send(`
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Listagem de Livros</h1>
-            </body>
-        </html>
-    `)
 })
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const app = express()
 
+const rotas = require('../app/rotas/rotas.js')
+
 module.exports = app;
