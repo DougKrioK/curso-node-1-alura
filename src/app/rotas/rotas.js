403f3e18f36c4f9eab0802959c03aa86bc290b6d
Douglas Pinheiro <douglas.pinheiro@icloud.com>
2019-03-24 13:37:45 -0300

    O padrão DAO

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,4 @@
+const LivroDao = require('../infra/livro-dao')
 const db = require('../../config/database')
 
 module.exports = (app) => {
@@ -16,7 +17,8 @@ module.exports = (app) => {
     })
     
     app.get('/livros', (req, resp) => {
-        db.all('SELECT * FROM livros', function(err, resultados){
+        const livroDao = new LivroDao(db);
+        livroDao.lista(function (err, resultados) {
 
             resp.marko(
                 require('../views/livros/lista/lista.marko'),
