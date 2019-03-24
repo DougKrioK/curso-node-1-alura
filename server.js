be06080815ad137c60c3b2de13e9e6171b84eaa5
Douglas Pinheiro <douglas.pinheiro@icloud.com>
2019-03-24 12:04:43 -0300

    Encapsulando o Express

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const app = require('./src/config/custom-express.js')
+
 app.listen(3000, function(){
     console.log('Servidor rodando na porta 3000');
     
@@ -30,34 +30,3 @@ app.get('/livros', (req, resp) => {
         </html>
     `)
 })
-// const http = require('http')
-
-// const servidor = http.createServer(function (req, resp) {
-//     let html = ''
-//     if(req.url == '/') {
-//         html = `
-//             <html>
-//                 <head>
-//                     <meta charset="utf-8">
-//                 </head>
-//                 <body>
-//                     <h1>Casa do Código</h1>
-//                 </body>
-//             </html>
-//         `
-
-//     } else if (req.url =='/livros') {
-//         html = `
-//             <html>
-//                 <head>
-//                     <meta charset="utf-8">
-//                 </head>
-//                 <body>
-//                     <h1>Listagem de Livros</h1>
-//                 </body>
-//             </html>
-//         `
-//     }
-//     resp.end(html)
-// })
-// servidor.listen(3000)
