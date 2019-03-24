227f1e158d700c14fe726ab880892bbf53059377
Douglas Pinheiro <douglas.pinheiro@icloud.com>
2019-03-24 14:06:31 -0300

    Middlewares

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -3,7 +3,11 @@ require("marko/node-require").install()
 require("marko/express")
 const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 const rotas = require('../app/rotas/rotas.js')
+
 
 module.exports = app;
