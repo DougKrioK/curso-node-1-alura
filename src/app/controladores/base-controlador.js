1644fedd5f9eb439b7f5c737d13f43af07a0649c
Douglas <douglaspinheiro@id.uff.br>
2019-03-25 09:54:41 -0300

    Iniciando a autenticação

--- src/app/controladores/base-controlador.js
+++ src/app/controladores/base-controlador.js
@@ -2,7 +2,8 @@ const templates = require('../views/templates');
 class BaseControlador {
     static rotas() {
         return {
-            home: '/'
+            home: '/',
+            login: '/login'
         };
     }
     
@@ -11,6 +12,22 @@ class BaseControlador {
             resp.marko(
                 templates.base.home
             );
+        };
+    }
+
+    login() {
+
+        return function (req, resp) {
+            resp.marko(templates.base.login);
+        };
+    }
+
+    efetuaLogin() {
+
+        return function (req, resp) {
+
+            // Lógica de login.
+
         };
     }
 }
